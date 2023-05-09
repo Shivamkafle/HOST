0570ca6e7849893370c6ee0fba42d93819e666c1
Shivamkafle <kafleshivam8@gmail.com>
2023-05-09 13:21:24 +0545

    Web Host

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("parent").innerHTML=("My name is Shivam Kafle")
+document.getElementById("parent").style.cssText=("background-color:red; color:black;text-align:center");
+document.getElementsByClassName("exercise")[0].innerHTML=("My name is Shivam Kafle")
+document.getElementsByClassName("exercise")[0].style.cssText=("background-color:red; color:black;text-align:center");
+document.getElementsByClassName("exercise")[1].style.cssText=("background-color:red; color:black;text-align:center");
+document.getElementsByClassName("exercise")[2].style.cssText=("background-color:red; color:black;text-align:center");
+document.getElementsByClassName("exercise")[3].style.cssText=("background-color:red; color:black;text-align:center");
+
+document.getElementsByTagName("h1")[0].style.cssText=("background-color:pink; color:black;text-align:center");
+
+document.getElementsByTagName("h1")[1].style.cssText=("background-color:black; color:black;text-align:center");
+document.getElementsByTagName("h1")[2].style.cssText=("background-color:yellow; color:black;text-align:center");
+document.getElementsByTagName("h1")[3].style.cssText=("background-color:red; color:black;text-align:center");
